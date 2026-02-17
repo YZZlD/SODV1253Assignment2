932c60f1fd89e812c5570a1bd2054f6fc61a05a6
YZZlD <max.verhulst@hotmail.com>
2026-02-17 15:45:55 -0700

    documentation: add explanation for design and architecture decisions

--- assignment2/src/App.jsx
+++ assignment2/src/App.jsx
@@ -4,50 +4,58 @@ import data from './data/data.json';
 import RecipeCard from './components/RecipeCard';
 
 function App() {
+  //State for recipes and diet.
   const [recipes, setRecipes] = useState([]);
   const [diet, setDiet] = useState("");
 
+  //Fetch from the spoonacular API with the ingredients list and dietary restrictions then set the recipes in state.
   async function FetchRecipes() {
-    //const queryString = `https://api.spoonacular.com/recipes/complexSearch?includeIngredients=${getIngredients()}${getDiet() == "" ? "" : `&diet=${getDiet()}`}&apiKey=${import.meta.env.VITE_API_KEY}`;
+    const queryString = `https://api.spoonacular.com/recipes/complexSearch?includeIngredients=${getIngredients()}${getDiet() == "" ? "" : `&diet=${getDiet()}`}&number=100&apiKey=${import.meta.env.VITE_API_KEY}`;
     const response = await fetch(queryString);
     const recipes = await response.json();
     setRecipes(recipes.results);
   }
 
+  //Helper function which checks which of the diet radio inputs are checked and grabs the value from it to determine the diet
   function getDiet() {
     return document.querySelector('input[name="diet"]:checked').value;
   }
 
+  //Grabs ingredients from the search bar value
   function getIngredients() {
     return document.getElementById("searchBar").value;
   }
 
   return (
     <>
-    <div>
-      <form>
-        <input type="search" id="searchBar" placeholder='ingredient1,ingredient2,ingredient3...'></input>
-        <fieldset>
-          <input type="radio" id="None" name="diet" value="" checked={"" == diet} onChange={() => setDiet("")}></input>
-          <label htmlFor="None">No Dietary Restrictions</label>
+      <div>
+        <form>
+          <input type="search" id="searchBar" placeholder='ingredient1,ingredient2,ingredient3...'></input>
+          {/* Radio button for dietary selection. Checked status is handled through a diet state variable, which is modified when the radio menu is clicked. */}
+          <fieldset>
+            <input type="radio" id="None" name="diet" value="" checked={"" == diet} onChange={() => setDiet("")}></input>
+            <label htmlFor="None">No Dietary Restrictions</label>
 
-          <input type="radio" id="Vegetarian" name="diet" value="vegetarian" checked={"vegetarian" == diet} onChange={() => setDiet("vegetarian")}></input>
-          <label htmlFor="Vegetarian">Vegetarian</label>
+            <input type="radio" id="Vegetarian" name="diet" value="vegetarian" checked={"vegetarian" == diet} onChange={() => setDiet("vegetarian")}></input>
+            <label htmlFor="Vegetarian">Vegetarian</label>
 
-          <input type="radio" id="Vegan" name="diet" value="vegan" checked={"vegan" == diet} onChange={() => setDiet("vegan")}></input>
-          <label htmlFor="Vegan">Vegan</label>
-        </fieldset>
-        <button type="button" onClick={FetchRecipes}>Search</button>
-      </form>
+            <input type="radio" id="Gluten-Free" name="diet" value="gluten-free" checked={"gluten-free" == diet} onChange={() => setDiet("gluten-free")}></input>
+            <label htmlFor="Gluten-Free">Gluten Free</label>
 
-    <div className='gridContainer'>
-      {recipes.map((recipe) => {
-        return <RecipeCard title={recipe.title} image={recipe.image}></RecipeCard>
-      })}
-    </div>
+            <input type="radio" id="Vegan" name="diet" value="vegan" checked={"vegan" == diet} onChange={() => setDiet("vegan")}></input>
+            <label htmlFor="Vegan">Vegan</label>
+          </fieldset>
+          <button type="button" onClick={FetchRecipes}>Search</button>
+        </form>
 
-    </div>
+        {/* Dynamically generate a grid of recipes returned from the API for a given query string. */}
+        <div className='gridContainer'>
+          {recipes.map((recipe) => {
+            return <RecipeCard title={recipe.title} image={recipe.image}></RecipeCard>
+          })}
+        </div>
 
+      </div>
     </>
   )
 }
--- assignment2/src/components/RecipeCard.jsx
+++ assignment2/src/components/RecipeCard.jsx
@@ -2,6 +2,7 @@ import '../css/recipeCard.css';
 
 export default function RecipeCard(props) {
     return (
+        //Display props information in recipe card.
         <>
             <div className="recipeCard">
                 <img className="recipeImage" src={props.image} alt={props.title}></img>
