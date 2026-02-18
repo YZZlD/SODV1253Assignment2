6421abe48a1e6e3b8f92280acc0b58a112209ab8
YZZlD <max.verhulst@hotmail.com>
2026-02-18 16:04:35 -0700

    feat: recipe cards can be expanded to show detailed information

--- assignment2/src/App.jsx
+++ assignment2/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import data from './data/data.json';
+import detailedData from'./data/detailedData.json'
 import RecipeCard from './components/RecipeCard';
 
 function App() {
   //State for recipes and diet.
   const [recipes, setRecipes] = useState([]);
   const [diet, setDiet] = useState("");
+  const[recipeDetails, addRecipeDetails] = useState({});
 
   //Fetch from the spoonacular API with the ingredients list and dietary restrictions then set the recipes in state.
   async function FetchRecipes() {
@@ -14,6 +16,18 @@ function App() {
     const response = await fetch(queryString);
     const recipes = await response.json();
     setRecipes(recipes.results);
+    //Testing without API calls
+    // setRecipes(data.results)
+  }
+
+  //Grab recipe detail from spoonacular API by id then add it to the recipeDetails object
+  const FetchRecipeDetails = async (id) =>{
+    const queryString = `https://api.spoonacular.com/recipes/${id}/information?apiKey=${import.meta.env.VITE_API_KEY}`;
+    const response = await fetch(queryString);
+    const detailedRecipe = await response.json();
+    addRecipeDetails({...recipeDetails, [id]: detailedRecipe});
+    //Testing without API calls
+    // addRecipeDetails({...recipeDetails, [id]: detailedData})
   }
 
   //Helper function which checks which of the diet radio inputs are checked and grabs the value from it to determine the diet
@@ -51,10 +65,11 @@ function App() {
         {/* Dynamically generate a grid of recipes returned from the API for a given query string. */}
         <div className='gridContainer'>
           {recipes.map((recipe) => {
-            return <RecipeCard title={recipe.title} image={recipe.image}></RecipeCard>
+            console.log(recipe.id);
+            if(recipeDetails[recipe.id]) return <RecipeCard key={recipeDetails[recipe.id].id} recipeId={recipeDetails[recipe.id].id} title={recipeDetails[recipe.id].title} image={recipeDetails[recipe.id].image} readyInMinutes={recipeDetails[recipe.id].readyInMinutes} instructions={recipeDetails[recipe.id].analyzedInstructions} ingredients={recipeDetails[recipe.id].extendedIngredients} fetchRecipeDetails={FetchRecipeDetails}></RecipeCard>;
+            else return <RecipeCard key={recipe.id} recipeId={recipe.id} title={recipe.title} image={recipe.image} ingredients={[]} instructions={[]} fetchRecipeDetails={FetchRecipeDetails}></RecipeCard>;
           })}
         </div>
-
       </div>
     </>
   )
--- assignment2/src/components/RecipeCard.jsx
+++ assignment2/src/components/RecipeCard.jsx
@@ -4,10 +4,31 @@ export default function RecipeCard(props) {
     return (
         //Display props information in recipe card.
         <>
-            <div className="recipeCard">
-                <img className="recipeImage" src={props.image} alt={props.title}></img>
-                <div className="recipeTitle">{props.title}</div>
-            </div>
+            <details className="recipeCard" onClick={() => props.fetchRecipeDetails(props.recipeId)}>
+                <summary>
+                    <div>
+                        <img className="recipeImage" src={props.image} alt={props.title}></img>
+                        <div className="recipeTitle">{props.title}</div>
+                    </div>
+                </summary>
+                <h3>Ready in ⏱ {props.readyInMinutes} minutes</h3>
+
+                {/* Loop over ingredients prop and display all ingredients */}
+                <ul>
+                    {props.ingredients.map((ingredient) => {
+                        return <li>{ingredient.original}</li>
+                    })}
+                </ul>
+
+                {/* Loop over instruction prop and display steps */}
+                <ol>
+                    {props.instructions.map((instruction) => {
+                        return instruction.steps.map((step) => {
+                            return <li>{step.step}</li>
+                        })
+                    })}
+                </ol>
+            </details>
         </>
     )
 }
